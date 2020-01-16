b6a59883c93c5990560ddda9e9f9663bcd0dd8bf
Yong Liu <yliu@nlpr.ia.ac.cn>
2020-01-16 10:37:29 +0800

    Add files via upload

---
+++ Classify_code/add_code_CV3/PCA_IN2ADNI.m
@@ -0,0 +1,30 @@
+   clear;clc
+% %PCA降维，降到492*491，按列对主成分进行排序
+addpath('C:\Users\Kunzh\Desktop\20200112\Classification\LDA')
+load('C:\Users\Kunzh\Desktop\20200112\Classification\IN_ADNI\label_adni');
+load('C:\Users\Kunzh\Desktop\20200112\Classification\IN_ADNI\label');
+load('C:\Users\Kunzh\Desktop\20200112\Classification\IN_ADNI\data_all');
+load('C:\Users\Kunzh\Desktop\20200112\Classification\IN_ADNI\data_adni');
+data_inad = [data_all;data_adni];
+label_inad = [label;label_adni'];
+%[data1,PS]=mapminmax(data,0,1)
+options = [ ];
+options.PCARatio = 1 ;
+[eigvector, eigvalue, meanData, data] = PCA(data_inad, options);
+
+
+train_data = data(1:length(label),:);
+test_data = data(length(label)+1:end,:);
+test_label = label_adni;
+train_label = label;
+for i =4
+    [model,k,ClassLabel]=LDATraining(train_data(:,1:i),train_label);
+    [tmp,target]=LDATesting(test_data(:,1:i),k,model,ClassLabel);
+    [TP,TN,FN,FP] = conclusion(target,test_label);
+    SEN(i) = TP / (TP + FN);
+    SPE(i) = TN / (TN + FP);
+    ACC(i) = (TP + TN) / (TP + FP + FN + TN);
+    [auc(i), curve] = rocplot(tmp(:,1)-tmp(:,2), test_label, 1, 0);
+end
+
+    
